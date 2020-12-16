388382a1b2cb7086165da2aeb1bcf0f23a69df8c
Matthew Gumport <m@gum.pt>
2020-12-15 18:32:51 -0800

    cleanup unused code

--- 04/02/src/main.rs
+++ 04/02/src/main.rs
@@ -3,7 +3,6 @@ extern crate lazy_static;
 use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::io::{self, BufRead};
-use std::str;
 
 #[derive(Debug)]
 struct Passport {
@@ -162,22 +161,10 @@ impl Passport {
 
         true
     }
-
-    // fn add_field(mut self, field: &str) {
-    //     self.fields.insert(field.to_string());
-    // }
 }
 
 fn part_two() {
     let stdin = io::stdin();
-
-    lazy_static! {
-        static ref REQUIRED_FIELDS: HashSet<&'static str> =
-            ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid", "cid"]
-                .iter()
-                .cloned()
-                .collect();
-    }
 
     let mut count = 0;
     let mut buf = vec![];
@@ -187,7 +174,6 @@ fn part_two() {
             // Process what's in the buffer
             let line = Passport::new(String::from_utf8(buf).unwrap());
             if line.is_valid() {
-                println!("{:?}", line);
                 count += 1;
             }
 
@@ -205,7 +191,6 @@ fn part_two() {
     // TODO Process what's left in the buffer
     let line = Passport::new(String::from_utf8(buf).unwrap());
     if line.is_valid() {
-        println!("{:?}", line);
         count += 1;
     }
 
