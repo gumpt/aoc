3611ff4aa9981dcad59d21f432555e11fc61a3d8
Matthew Gumport <m@gum.pt>
2021-12-21 00:12:10 -0800

    day 02

---
+++ 2021/02/01/src/main.rs
@@ -0,0 +1,49 @@
+use std::io::{self, BufRead};
+
+#[derive(Debug)]
+struct Submarine {
+    depth: i64,
+    horizontal: i64,
+}
+
+impl Submarine {
+    fn new() -> Submarine {
+        Submarine {
+            depth: 0,
+            horizontal: 0,
+        }
+    }
+
+    fn forward(&mut self, distance: i64) {
+        self.horizontal += distance;
+    }
+
+    fn up(&mut self, distance: i64) {
+        self.depth -= distance;
+    }
+
+    fn down(&mut self, distance: i64) {
+        self.up(distance * -1);
+    }
+}
+
+fn main() {
+    let mut sub = Submarine::new();
+
+    let stdin = io::stdin();
+    for unwrapped in stdin.lock().lines() {
+        let line = unwrapped.unwrap();
+        let splat: Vec<&str> = line.split(' ').take(2).collect();
+
+        let (action, distance) = (splat[0], splat[1].parse::<i64>().unwrap());
+        match action {
+            "up" => sub.up(distance),
+            "down" => sub.down(distance),
+            "forward" => sub.forward(distance),
+            _ => println!("NUPE"),
+        }
+    }
+
+    println!("{:?}: {}", sub, sub.depth * sub.horizontal);
+    // println!("PART 1: {}", count);
+}
